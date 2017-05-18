b61eb0ef8a7104982b153707d02e559c0d16db73
Luis Andrade <rodesiao@gmail.com>
2017-05-17 22:26:59 -0300

    Filtro de login

---
+++ src/filter/LoginFilter.java
@@ -0,0 +1,56 @@
+package filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.Usuario;
+
+@WebFilter("/controller.do")
+public class LoginFilter implements Filter {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpSession session = req.getSession();
+		Usuario usuario = (Usuario)session.getAttribute("logado");
+		String command = req.getParameter("command");
+		String path = req.getContextPath();
+		String uri = req.getRequestURI();
+						
+		if (usuario == null && !uri.equals(path + "/login.jsp") && !command.equals("FazerLogin")) {
+			((HttpServletResponse) response).sendRedirect(path + "/login.jsp");
+		}
+		else {
+			chain.doFilter(request, response);
+		}
+		
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(FilterConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
